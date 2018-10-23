236f79d938680f76be6e165e2d6be92ab92679bd
Colin Gross <grosscol@gmail.com>
2018-10-23 14:24:52 -0400

    Only use causal pathway examples.   Clean out class and predicate approaches to ISRs.

--- example/predicate/query_isr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Assumes fuseki-server with dataset named 'ds'
-# and updated by ISR
-
-curl --data-binary @result.sparql \
-  --header 'Content-type: application/sparql-query' \
-  'http://localhost:3030/ds/query' | jq .
